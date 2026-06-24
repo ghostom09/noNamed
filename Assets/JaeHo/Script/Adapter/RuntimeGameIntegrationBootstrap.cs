79c411a6df195b540429dabd5c13c159a212ed65
bagdad0128 <pjho3371@gmail.com>
2026-06-24 22:36:50 +0900

    버그 수정

--- Assets/JaeHo/Script/Adapter/RuntimeGameIntegrationBootstrap.cs
+++ Assets/JaeHo/Script/Adapter/RuntimeGameIntegrationBootstrap.cs
@@ -378,7 +378,7 @@ public sealed class RuntimeGameIntegrationRunner : MonoBehaviour
         }
 
         BossBase activeBoss = FindActiveBoss();
-        if (bossHpBar != null && activeBoss != null && !bossHpBar.HasLiveBoss)
+        if (bossHpBar != null && activeBoss != null)
         {
             bossHpBar.SetBoss(activeBoss);
         }
